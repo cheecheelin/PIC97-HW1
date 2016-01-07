119b1e9283e9bba7f769fa7153001be61f278813
cheecheelin <cheecheelin@s-169-232-112-198.resnet.ucla.edu>
2016-01-07 15:34:38 -0800

    HW1

---
+++ hw1.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+#Cheechee Lin PIC 97 HW 1 01/07/2016 
+#Newton's Method for given test cases 
+
+import math
+
+#hardcoded functions and their derivatives 
+def f(x):
+	return x**2-1
+def df(x):
+	return 2*x-1
+def g(x):
+	return math.sin(x)
+def dg(x):
+	return math.cos(x)
+def h(x):
+	return math.log1p(x-1)-1
+def dh(x):
+	return 1/x
+
+#Newton's Method 
+def newton(func,dfunc,x,eps):
+	xi=x
+	#if magnitude of residual error is less than or equal to eps then done, else calculate correction to guess
+	while abs(func(xi))>eps:
+		xi= xi-(func(xi)/dfunc(xi))
+		if abs(func(xi)-func(x))<=eps:
+			break
+	return xi
+
+
+print newton(f,df,3,0.001)
+print newton (f,df,-0.5,0.001)
+print newton (g,dg,2,0.001)
+print newton (h,dh,1.5,0.001)
